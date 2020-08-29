86b562955faa16830588e5475fa9c15985c81f31
Loraine Vilches <lorainevilches@gmail.com>
2020-08-28 22:45:40 -0300

    Update login.js
    change variables to let and const

--- script/login.js
+++ script/login.js
@@ -1,7 +1,7 @@
 //Add array of data or call an API
 
 //Here we gonna do with fixed data
-var accessData = [
+const accessData = [
     {
         login: "convidado",
         pass: "senhaconvidado"
@@ -10,7 +10,7 @@ var accessData = [
 
 //Function to show the Modal
 function noAccessShow(){
-    var modalNoAccess = document.getElementById("staticBackdrop");
+    let modalNoAccess = document.getElementById("staticBackdrop");
     if(modalNoAccess.style.display = "none"){
         modalNoAccess.style.display = "block";
     }
@@ -18,7 +18,7 @@ function noAccessShow(){
 
 //Function to hide the Modal
 function noAccessHide(){
-    var modalNoAccess = document.getElementById("staticBackdrop");
+    let modalNoAccess = document.getElementById("staticBackdrop");
     if(modalNoAccess.style.display = "block"){
         modalNoAccess.style.display = "none";
         document.getElementById("formFull").reset();
@@ -27,8 +27,8 @@ function noAccessHide(){
 
 
 function tryEnter() {
-    var login = document.getElementById("inputLogin").value;
-    var pass = document.getElementById("inputPass").value;
+    let login = document.getElementById("inputLogin").value;
+    let pass = document.getElementById("inputPass").value;
     for(i = 0; i < accessData.length; i++){
         if(login == accessData[i].login && pass == accessData[i].pass){
             console.log("Done");
